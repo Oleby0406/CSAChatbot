ea725a1b245240c59ae0fcc80411b05b9814e470
Oleby0406 <oleg.bychenkov@gmail.com>
2021-03-23 17:00:18 -0700

    first version of main loop and greetingCheck()

--- chatbot.java
+++ chatbot.java
@@ -1,8 +1,17 @@
-import Java.util;
+import java.util.Scanner;
 public class chatbot{
-    String chatbotAnswer;
-    String userInput;
+    static String chatbotAnswer = "";
+    static String userInput = "";
     public static void main(String []args){
+        System.out.println("Hello");
+        Scanner scanner = new Scanner(System.in);
+        while (!userInput.equals("stop")) {
+            userInput = scanner.nextLine().toLowerCase();
+            chatbotAnswer = "";
+            greetingCheck(userInput);
+            System.out.println(chatbotAnswer);
+        }
+
         //while loop
         //every iteration get a scanner to get input, make it lowercase and put that into userInput
         //clear the final answer, run all the checks we got to form the final answer
@@ -10,15 +19,19 @@ public class chatbot{
         //break loop when a certain phrase is typed like "stop" or "break"
     }
 
-    public static greetingCheck(String input) {
-        String[] checks = {"hello", "hi", "hey"} //add more here, also keep all lowercase for simplicity
+    public static void greetingCheck(String input) {
+        String[] checks = {"hello", "hi", "hey"}; //add more here, also keep all lowercase for simplicity
 
         //make a couple of answers in an array
-        String[] answers = {"Hey, Nice to meet you!", "Hello!"} //add more, current are just ideas
+        String[] answers = {"Hey, Nice to meet you!", "Hello!"}; //add more, current are just ideas
 
+        for (int i = 0; i < checks.length; i++) {
+            if (userInput.contains(checks[i])) {
+                chatbotAnswer += answers[(int) (Math.random()*checks.length - 1)];
+                break;
+            }
+        }
         //loop through array and check for a greeting word here from the checks array using userInput
-
-        //chatbotAnswer += answers[Math.random()*2] or smthn like that
         //concatenate the answer from this check to the final answer
     }
 
