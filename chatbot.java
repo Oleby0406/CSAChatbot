2e4e37e2107d363db97ed4a52afee85b833aee78
Oleby0406 <oleg.bychenkov@gmail.com>
2021-03-23 22:34:37 -0700

    final hopefully

--- chatbot.java
+++ chatbot.java
@@ -7,7 +7,7 @@ public class chatbot {
 
     static String chatbotAnswer = "";
     static String userInput = "";
-    static boolean metAlready = false;
+    static boolean questionAsked = false;
 
     public static void main(String []args) {
         System.out.println("Type anything in to get a response. Type in 'stop' without the quotes to exit.");
@@ -16,21 +16,18 @@ public class chatbot {
 
             userInput = scanner.nextLine().toLowerCase();
             chatbotAnswer = "";
-
+            
+            reactToAnswer();
             greetingCheck(userInput);
             feelingCheck(userInput);
             likeCheck(userInput);
             hateCheck(userInput);
+
             confusedCheck(userInput);
+
+            askQuestion();
             System.out.println("Bot: " + chatbotAnswer);
         }
-
-        // while loop
-        // every iteration get a scanner to get input, make it lowercase and put that
-        // into userInput
-        // clear the final answer, run all the checks we got to form the final answer
-        // print final answer
-        // break loop when a certain phrase is typed like "stop" or "break"
     }
 
     private static HashMap<String, Double> sentiment = new HashMap<String, Double>();
@@ -118,15 +115,10 @@ public class chatbot {
         String[] checks = {"hello", "hi", "hey", "greetings", "good morning", "what's up"};
 
         String[] answers = {"Hey, nice to meet you!", "Hello, can't wait to talk to you!"};
-        if (metAlready) {
-            chatbotAnswer += "I believe we have already met each other, let's talk about something else. ";
-        } else {
-            for (int i = 0; i < checks.length; i++) {
-                if (userInput.contains(checks[i])) {
-                    chatbotAnswer += answers[(int) (Math.random()*answers.length - 1)] + " ";
-                    metAlready = true;
-                    break;
-                }
+        for (int i = 0; i < checks.length; i++) {
+            if (userInput.contains(checks[i])) {
+                chatbotAnswer += answers[(int) (Math.random()*answers.length - 1)] + " ";
+                break;
             }
         }
     }
@@ -158,7 +150,7 @@ public class chatbot {
                     break;
                 }
             }
-            chatbotAnswer += "I like " + answer + " too!";
+            chatbotAnswer += "I like " + answer + " too! ";
         }
     }
 
@@ -172,15 +164,31 @@ public class chatbot {
                     break;
                 }
             }
-            chatbotAnswer += "I am not a big fan of " + answer + " as well to be honest.";
+            chatbotAnswer += "I am not a big fan of " + answer + " as well to be honest. ";
         }
     }
 
     public static void confusedCheck(String input) {
-        String[] answers = {"One day on Venus is longer than one year on Earth", "The first person convicted of speeding was going eight mph.", "The Nobel Peace Prize is named for Alfred Nobel, the inventor of dynamite."};
+        String[] answers = {"One day on Venus is longer than one year on Earth", "The first person convicted of speeding was going eight mph.", "The Nobel Peace Prize is named for Alfred Nobel, the inventor of dynamite.", "It's illegal to own just one guinea pig in Switzerland", "The average person spends two weeks of their life waiting at traffic lights."};
         if (chatbotAnswer.equals("")) {
             chatbotAnswer += "Sorry, I don't understand what you're saying. Here's a fun fact instead: ";
             chatbotAnswer += answers[(int) (Math.random()*answers.length - 1)] + " ";
+        }
+    }
+
+    public static void askQuestion() {
+        String[] questions = {"How are you doing today?", "What's the weather like where you live?", "What is your favorite hobby?", "Is cereal a soup?", "If a tree falls in a forest and no one is around to hear it, does it make a sound?", "What's the most useless thing you own?", "Do you have a pet?"};
+        if (userInput.length() < 10) {
+            questionAsked = true;
+            chatbotAnswer += "I have a question for you: ";
+            chatbotAnswer += questions[(int) (Math.random()*questions.length - 1)] + " ";
+        }
+    }
+    public static void reactToAnswer() {
+        String[] reactions = {"That's cool!", "Interesting!", "Tell me more.", "Thanks for sharing!"};
+        if (questionAsked) {
+            chatbotAnswer += reactions[(int) (Math.random()*reactions.length - 1)] + " ";
+            questionAsked = false;
         }
     }
 }   
